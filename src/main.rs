92f6764c1fdd004b9420ef187077c3c7f861a293
Tomás Aguiar <aguiarhcode@gmail.com>
2022-05-26 23:55:09 +0100

    Generate a random number, get the input from the user and comparing it to the secret_key generated by the program.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use std::io;
+use rand::Rng;
+use colored::*;
+use std::cmp::Ordering;
 
 fn main() {
     println!("The Guessing Game");
     println!("=================");
 
+    let secret_key = rand::thread_rng().gen_range(1, 101);
+
+    // println!("your secret number is {}", secret_key);
     println!("Guess a number between 1 and 100");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
     
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            // if input ins't a number, return an error.
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_key) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            },
+        }}
 }
